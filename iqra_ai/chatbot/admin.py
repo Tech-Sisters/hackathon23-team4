46b1f13a0e3070e21a86df49998cb633d6452aab
Tarctic <zainabshah2003@gmail.com>
2023-11-17 23:27:12 +0300

    admin setup

--- iqra_ai/chatbot/admin.py
+++ iqra_ai/chatbot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, UserProfile, Word, Lesson, Level, Message
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Word)
+admin.site.register(Lesson)
+admin.site.register(Level)
+admin.site.register(Message)
