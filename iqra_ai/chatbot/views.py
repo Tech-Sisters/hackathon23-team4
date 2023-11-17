b08af21b1f60df3a0a5c2db54aa752e5030d54bb
Tarctic <zainabshah2003@gmail.com>
2023-11-18 02:46:50 +0300

    add index template

--- iqra_ai/chatbot/views.py
+++ iqra_ai/chatbot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import User, UserProfile, Word, Lesson, Level, Message
+
 # Create your views here.
+def index(request):
+
+    return render(request, "chatbot/index.html")
