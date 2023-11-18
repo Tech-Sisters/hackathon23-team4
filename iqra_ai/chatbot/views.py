a1059d59139caf6606184ad4727acda61835f92e
Tarctic <zainabshah2003@gmail.com>
2023-11-18 21:56:24 +0300

    Login full setup

--- iqra_ai/chatbot/views.py
+++ iqra_ai/chatbot/views.py
@@ -1,8 +1,100 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
 
 from .models import User, UserProfile, Word, Lesson, Level, Message
 
-# Create your views here.
+# Landing/Chatbot page
 def index(request):
+    """
+    Redirects the user to the chatbot page if authenticated,
+    otherwise, directs them to the landing page.
+    """
 
-    return render(request, "chatbot/index.html")
+    if request.user.is_authenticated:
+        return render(request, "chatbot/chatbot.html")
+    
+    else:
+        return render(request, "chatbot/index.html")
+       
+
+# Login page
+def login_view(request):
+    """
+    Handles user login. Displays the login form and authenticates 
+    the user upon form submission.
+    """
+
+    if request.method == "POST":
+        # User submitted the login form
+
+        # Attempt to sign the user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication was successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "chatbot/login.html", {
+                "message": "Invalid username and/or password."
+            })
+        
+    else:
+        # Display the login form
+        return render(request, "chatbot/login.html")
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    """
+    Handles user registration. 
+    Displays the registration form.
+    Creates a new user upon successful form submission. 
+    """
+
+    if request.method == "POST":
+        # User submitted the registration form
+
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "chatbot/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create a new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+
+        # Ensure username is unique
+        except IntegrityError:
+            return render(request, "chatbot/register.html", {
+                "message": "Username already taken."
+            })
+        
+        
+        # Create a user profile
+        profile = UserProfile(user=user)
+        profile.save()
+
+        # Log in the user
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    
+    else:
+        # Display the registration form
+        return render(request, "chatbot/register.html")
+    
