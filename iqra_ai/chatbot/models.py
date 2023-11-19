05b88a4b1f4301a3ef469478a0ede4221af21d12
Tarctic <zainabshah2003@gmail.com>
2023-11-19 23:15:56 +0300

    add part_of_speech Word attribute and migrate 2

--- iqra_ai/chatbot/models.py
+++ iqra_ai/chatbot/models.py
@@ -13,6 +13,7 @@ class Word(models.Model):
 
     word = models.CharField(max_length=50)
     frequency = models.PositiveIntegerField(default=0)
+    part_of_speech = models.CharField(max_length=50, default="noun")
     word_type = models.CharField(max_length=6, choices=TypeChoices.choices, default=TypeChoices.FREE)
 
     def __str__(self):
