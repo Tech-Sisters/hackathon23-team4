12d68d5d7acafb11eb623c59bef59538c3a8f0fc
Tarctic <zainabshah2003@gmail.com>
2023-11-21 09:13:00 +0300

    improve bubble UI/UX

--- iqra_ai/chatbot/static/chatbot/chatbot.js
+++ iqra_ai/chatbot/static/chatbot/chatbot.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function do_something() {
     const chatMessages = document.querySelector('.chat-messages');
     const userInput = document.getElementById('userInput');
+    let bubbleContainer = document.getElementById('bubbleContainer')
 
     function addUserMessage(message) {
         const userMessage = document.createElement('div');
@@ -16,11 +17,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function addAssistantMessage(message) {
+        // Remove existing bubbles
+        if (bubbleContainer) {
+            bubbleContainer.remove();
+        }
+
         const assistantMessage = document.createElement('div');
         assistantMessage.classList.add('message', 'assistant-message');
-        assistantMessage.textContent = message;
+        const words = message.split(' ');
+
+        bubbleContainer = document.createElement('div');
+        bubbleContainer.id = 'bubbleContainer';
+        bubbleContainer.style.position = 'relative';
+        assistantMessage.appendChild(bubbleContainer);
+
+        words.forEach((word) => {
+            const span = document.createElement('span');
+            span.textContent = word + ' ';
+            span.classList.add('clickable-word');
+            span.addEventListener('click', (event) => createBubble(event, word));
+            bubbleContainer.appendChild(span);
+        });
+
         chatMessages.appendChild(assistantMessage);
         chatMessages.scrollTop = chatMessages.scrollHeight; // Auto-scroll to the bottom
+    }
+
+    function createBubble(event, word) {
+        // Remove existing bubbles
+        const existingBubbles = document.querySelectorAll('.bubble');
+        existingBubbles.forEach((bubble) => bubble.remove());
+
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+        bubble.textContent = word;
+        document.body.appendChild(bubble);
+
+        // Calculate position above the clicked word
+        const rect = event.target.getBoundingClientRect();
+        bubble.style.top = rect.top - bubble.offsetHeight - 10 + 'px';
+        bubble.style.left = rect.left + 'px';
+
+        // Remove the bubble after a short delay
+        setTimeout(() => {
+            bubble.remove();
+        }, 2000);
     }
 
     function processUserInput() {
@@ -29,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
             addUserMessage(userMessage);
             // Simulate a bot response (you can replace this with actual API call or other logic)
             setTimeout(() => {
-                addAssistantMessage('This is a bot response.');
+                addAssistantMessage('This is a bot response with clickable words.');
             }, 500);
             userInput.value = '';
         }
